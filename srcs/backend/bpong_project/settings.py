4a3188d0e5749b0a7bc5011d2e770594362ab856
Lionel Clerc <lionel.clerc@protonmail.com>
2024-07-24 16:24:39 +0300

    feat: Integrate ELK stack for backend logging and indexing
    - Updated docker-compose.yml to include volumes and configurations for Filebeat and Logstash
    - Configured Filebeat to collect and forward backend logs
    - Modified logstash.conf to process and index logs from Filebeat
    - Updated Django settings.py to log application events to app.log
    - Cleaned up and removed unnecessary log files from elk/filebeat_ingest_data
    - Ensured proper volume mounting for log directories
    
    This commit enables backend logging via Filebeat and indexing by Logstash, improving log management and observability.

--- srcs/backend/bpong_project/settings.py
+++ srcs/backend/bpong_project/settings.py
@@ -1,4 +1,3 @@
-# settings.py
 
 from pathlib import Path
 import os
@@ -17,8 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure--5o3-*y!2w25g-%9h^8yt!z(!7f^p_xf8+x5u(9z#*^o#pxtq9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -134,3 +133,48 @@ LOGIN_URL = 'accounts:login'
 # List of trusted origins for CSRF protection
 # Requests from these origins will be allowed to bypass the CSRF protection
 CSRF_TRUSTED_ORIGINS = ['https://localhost']
+
+################################################################################
+## Django setting for Logging 
+################################################################################
+import os
+import logging.config
+
+# Create a logs directory if it doesn't exist
+LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+os.makedirs(LOGS_DIR, exist_ok=True)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOGS_DIR, 'app.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+import logging
+logger = logging.getLogger('django')
+
+logger.debug('DEBUG: Test log entry for Django in settings.py')
