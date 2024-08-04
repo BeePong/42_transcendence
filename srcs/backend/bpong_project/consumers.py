e2de4f04fbe7e075047e62fb00a6288afbecc42e
Valeria Vagapova <lera.vagapova@gmail.com>
2024-08-04 17:57:54 +0300

    add functions and buttons for testing websocket

--- srcs/backend/bpong_project/consumers.py
+++ srcs/backend/bpong_project/consumers.py
@@ -7,3 +7,32 @@ class PongConsumer(WebsocketConsumer):
 
     def disconnect(self, close_code):
         pass
+    
+    def send_message(self, message):
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+    def handle_tournament_message(self, text_data_json):
+        self.send_message('Received tournament message: ' + text_data_json['message'])
+
+    def handle_game_message(self, text_data_json):
+        self.send_message('Received game message: ' + text_data_json['message'])
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message_type = text_data_json['type']
+
+        if message_type == 'tournament':
+            self.handle_tournament_message(text_data_json)
+        elif message_type == 'game':
+            self.handle_game_message(text_data_json)
+        else:
+            self.send(text_data=json.dumps({
+                'error': 'Invalid message type'
+            }))
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        self.send_message('Received your message: ' + message)
