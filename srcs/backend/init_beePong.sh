3bf6f8c2ae9d9987893b3777b7898ef242a62396
linh nguyen <thuynguy@student.hive.fi>
2024-08-17 22:08:44 +0300

    nginx port value are fetched from .env

--- srcs/backend/init_beePong.sh
+++ srcs/backend/init_beePong.sh
@@ -2,6 +2,7 @@
 
 # Modify the database so it can store the data associated with any new models we’ve defined.
 python manage.py makemigrations beePong
+python manage.py makemigrations 
 # Apply the migration (if any) and have Django modify the database for us accordingly.
 
 python manage.py migrate
