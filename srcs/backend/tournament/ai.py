15e3f2c7049fa912a96956b4e5e8d21a5b27db4d
linh nguyen <thuynguy@student.hive.fi>
2024-08-24 23:03:57 +0300

    AI bot now reaches the server but 403, debug WIP

--- srcs/backend/tournament/ai.py
+++ srcs/backend/tournament/ai.py
@@ -1,9 +1,23 @@
 import asyncio
 import websockets
+import ssl
+import pathlib
+
 
 async def ai_bot():
-    url = "wss://localhost/ws/pong/1/?is_bot=True"
-    async with websockets.connect(url) as websocket:
+    url = "wss://nginx/ws/pong/1/?is_bot=True"
+
+    # Create an SSL context that does not verify certificates (for testing purposes)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.load_verify_locations(
+        "/etc/nginx/ssl/beepong.pem"
+    )  # Adjust this path if needed
+
+    async with websockets.connect(url, ssl=ssl_context) as websocket:
+        # async with websockets.connect(url, ssl=ssl_context) as websocket:
         await websocket.send("Hello, I'm an AI bot!")
+        response = await websocket.recv()
+        print(f"Received: {response}")
+
 
 asyncio.get_event_loop().run_until_complete(ai_bot())
