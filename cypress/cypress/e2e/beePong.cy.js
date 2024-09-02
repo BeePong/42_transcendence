a1d64432528353e46c4d3ca42f19e158bcc55126
Lionel Clerc <lionel.clerc@protonmail.com>
2024-09-02 14:33:55 +0300

    test(cypress): add basic end-to-end tests for key routes and components
    - Added tests for homepage, about page, game, API, and health endpoints.
    - Verified presence of specific content on each page.
    - Ensured navigation and content integrity across core routes.

--- cypress/cypress/e2e/beePong.cy.js
+++ cypress/cypress/e2e/beePong.cy.js
@@ -1,11 +1,36 @@
 describe('Sample Test', () => {
-  it('should visit the homepage and contains about', () => {
+  it('test visits the `homepage` and contains `about`', () => {
     cy.visit('/');
     cy.contains('ABOUT');
   });
 
-  it('should visit the about page and contains about BeePong', () => {
+  it('test visits the about `page` and contains `about BeePong`', () => {
     cy.visit('/about');
     cy.contains('about BeePong');
+  });
+
+  it('test visits the `api` and contains `{"test": "This is a test JSON"}`', () => {
+    cy.visit('/api');
+    cy.contains('{"test": "This is a test JSON"}');
+  });
+
+  it('test visits the `game` and contains `game`', () => {
+    cy.visit('/game');
+    cy.contains('game');
+  });
+
+  it('test visits the `health` and contains `{"status": "healthy"}`', () => {
+    cy.visit('/health');
+    cy.contains('{"status": "healthy"}');
+  });
+
+  it('test visits the `home` page and contains `*created by 5 ultra talanted Hive Helsinki students. Tech stuff here ->`', () => {
+    cy.visit('/home');
+    cy.contains("*created by 5 ultra talanted Hive Helsinki students. Tech stuff here ->");
+  });
+
+  it('test visits the `navbar` and contains `about`', () => {
+    cy.visit('/navbar');
+    cy.contains('ABOUT');
   });
 });
