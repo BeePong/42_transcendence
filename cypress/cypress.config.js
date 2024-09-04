de81bf625c4a061a75a11672c363cbffdaa4eb30
Lionel Clerc <lionel.clerc@protonmail.com>
2024-09-04 13:07:00 +0300

    feat: Implement logging with cy.task for Cypress tests

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   e2e: {
-    baseUrl:  'https://nginx',
-		setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
-  }
+    baseUrl: 'https://nginx',
+    setupNodeEvents(on, config) {
+      // Implement the logging task
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+
+      // If you have any other event listeners, they can go here as well
+    },
+  },
 };
+
