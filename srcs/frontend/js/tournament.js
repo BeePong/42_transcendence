f97db669ae3df2c66a01fd9db4e4a1aab532b9ff
Diego James <die.james.talamas@gmail.com>
2024-09-10 15:47:45 +0300

    Coment the description in solo game

--- srcs/frontend/js/tournament.js
+++ srcs/frontend/js/tournament.js
@@ -92,9 +92,9 @@ function handleFullTournamentLobby(player1_alias, player2_alias) {
   document.getElementById("tournament-lobby-section").classList.add("full");
   document.querySelector(".tournament_lobby__header").innerHTML =
     'BEEPONG CUP IS STARTING IN <span id="countdown">3</span>...';
-  document.querySelector(
-    ".tournament_lobby__description"
-  ).innerHTML = `<span id="player1">${player1_alias}</span> vs <span id="player2">${player2_alias}</span>`;
+  //document.querySelector(
+  //  ".tournament_lobby__description"
+  //).innerHTML = `<span id="player1">${player1_alias}</span> vs <span id="player2">${player2_alias}</span>`;
   const playerCount = document.querySelector(".tournament_lobby__player-count");
   if (playerCount) {
     playerCount.remove();
